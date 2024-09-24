93aa7f80619a624e75d3d86714dfdf4301b7730d
lexaDOOM <rfvghm@gmail.com>
2024-09-24 22:26:36 +0300

    Added week01/task03

---
+++ week01/task03/src/main/java/edu/penzgtu/App.java
@@ -0,0 +1,20 @@
+package edu.penzgtu;
+import java.util.Scanner;
+import java.lang.Math;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length of dodecahedron >> ");
+        float length = input.nextFloat();
+        System.out.println(dodecahedron_volume(length));
+        input.close();
+    }
+
+    // Method which calculates dodddecahedron volume
+    private static double dodecahedron_volume(double length)
+    {
+        return (Math.pow(length, 3) / 4) * (15 + (7 * Math.sqrt(5)));
+    }
+}
