baebbdfd66496f27e36e131f7e725ea8f4aa58c8
lexandrovitsch <eeeprohor@yandex.ru>
2024-10-24 20:32:23 +0300

    Added soluton for task_03

--- week05/task_03/src/main/java/edu/penzgtu/App.java
+++ week05/task_03/src/main/java/edu/penzgtu/App.java
@@ -1,11 +1,45 @@
 package edu.penzgtu;
 
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+        System.out.print("Enter sensors data >> ");
+        String input = scanner.nextLine();
+
+        if (input.length() > 512) {
+            input = input.substring(0, 512);
+        }
+
+        String[] sensorsData = input.split("@");
+
+        Sensors processor = new Sensors(100); 
+
+        try {
+            processor.sensorDataProc(sensorsData);
+        } catch (InvalidSensorDataException error) {
+            System.out.println("Error " + error.getMessage());
+            System.exit(1);
+        }
+        
+
+        double[] averageTemps = processor.calculateAverageTemps();
+
+        System.out.print("Sort by: 1 - IDs, 2 - Temperature >> ");
+        int sortChoice = scanner.nextInt();
+        scanner.close();
+
+        if (sortChoice == 1) {
+            processor.sortById(averageTemps);
+        } else if (sortChoice == 2) {
+            processor.sortByTemp(averageTemps);
+        } else {
+            System.out.println("Wrong choice!");
+            System.exit(0);
+        }
+
+        processor.printResults(averageTemps);
     }
 }
---
+++ week05/task_03/src/main/java/edu/penzgtu/InvalidSensorDataException.java
@@ -0,0 +1,7 @@
+package edu.penzgtu;
+
+public class InvalidSensorDataException extends Exception {
+    public InvalidSensorDataException(String message) {
+        super(message);
+    }
+}
